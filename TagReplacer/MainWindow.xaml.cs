468ef95679fc0cb951b27e30a4f1ae1b1c87e286
Yongjun <yongjun042@gmail.com>
2020-08-18 12:49:11 +0900

    Add project files.

---
+++ TagReplacer/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TagLib;
+
+namespace TagReplacer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class MusicTag
+        {
+            private string FilePath { get; set; }
+            private string Artist { get; set; }
+            private string Title { get; set}
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Audio (*.mp3,*.wav,*flac...)|*.mp3;*.wav;*.flac|All Files (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Read the files
+                foreach (String file in openFileDialog.FileNames)
+                {
+                    // Create a PictureBox.
+                    try
+                    {
+                        var tfile = TagLib.File.Create(file);
+
+                    }
+                    catch (SecurityException ex)
+                    {
+                        // The user lacks appropriate permissions to read files, discover paths, etc.
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                            "Error message: " + ex.Message + "\n\n" +
+                            "Details (send to Support):\n\n" + ex.StackTrace
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        // Could not load the image - probably related to Windows file system permissions.
+                        MessageBox.Show("Cannot load the music: " + file.Substring(file.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
